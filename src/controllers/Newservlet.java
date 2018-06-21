d29fd8bbd2e76996f0e9ff7c016fb2f1e8665a3a
kashimada.maki <kashimada.maki@videomarket.co.jp>
2018-06-21 12:46:08 +0900

    Add NewServlet.java

---
+++ src/controllers/Newservlet.java
@@ -0,0 +1,53 @@
+package controllers;
+
+import java.io.IOException;
+
+import javax.persistence.EntityManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.Task;
+import utils.DBUtil;
+
+/**
+ * Servlet implementation class Newservlet
+ */
+@WebServlet("/new")
+public class Newservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Newservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    EntityManager em = DBUtil.createEntityManager();
+	    em.getTransaction().begin();
+
+	    Task t = new Task();
+
+	    String title = "taro";
+	    t.setTitle(title);
+
+	    String content = "hello";
+	    t.setContent(content);
+
+	    em.persist(t);
+	    em.getTransaction().commit();
+
+		response.getWriter().append(Integer.valueOf(t.getId()).toString());
+
+		em.close();
+	}
+
+}
